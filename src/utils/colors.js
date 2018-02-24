25e3326cefb88f6fa3f31ace8dbec99986fb29b6
tomhuhges <merzbear@gmail.com>
2018-02-24 14:12:43 +0000

    commit before removing storybook

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,6 +1,5 @@
-const colors = (cssProp) => (props, baseColor = 'black', baseShade = 'base', transform) => {
+const colors = (cssProp) => ({ theme }, baseColor = 'black', baseShade = 'base', transform) => {
 
-  const { theme } = props;
   const colorNames = Object.keys(theme.colors);
   const shadeNames = Object.keys(theme.colors.default);
 
@@ -12,8 +11,9 @@ const colors = (cssProp) => (props, baseColor = 'black', baseShade = 'base', tra
     const shades = shadeNames.filter(shade => props[shade]);
     const shade = shades.length === 0 ? baseShade : shades[shades.length - 1];
 
-    return `${cssProp} : ${transform ? transform(0.05, theme.colors[color][shade]) :
-      theme.colors[color][shade]};`;
+    return `${cssProp} : ${transform
+      ? transform(0.05, theme.colors[color][shade])
+      : theme.colors[color][shade]};`;
   }
 }
 
