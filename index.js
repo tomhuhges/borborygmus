ada417fdef2ae3b1f9c8438a7e03cad21bccd972
tomhuhges <merzbear@gmail.com>
2018-02-27 00:41:27 +0000

    move index to root

---
+++ index.js
@@ -0,0 +1,23 @@
+export P from './src/components/Typography/P';
+export A from './src/components/Typography/A';
+export H1 from './src/components/Typography/H1';
+export H2 from './src/components/Typography/H2';
+export H3 from './src/components/Typography/H3';
+export H4 from './src/components/Typography/H4';
+export H5 from './src/components/Typography/H5';
+export H6 from './src/components/Typography/H6';
+export Span from './src/components/Typography/Span';
+export Em from './src/components/Typography/Em';
+export Strong from './src/components/Typography/Strong';
+export Pre from './src/components/Typography/Pre';
+export Figcaption from './src/components/Typography/Figcaption';
+export Blockquote from './src/components/Typography/Blockquote';
+export Ul from './src/components/Typography/Ul';
+export ULi from './src/components/Typography/ULi';
+export Ol from './src/components/Typography/Ol';
+export OLi from './src/components/Typography/OLi';
+export Button from './src/components/Typography/Button';
+
+export Figure from './src/components/Media/Figure';
+
+export Div from './src/components/Layout/Div';
